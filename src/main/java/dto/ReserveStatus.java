79d74d8dff45082f11aa578a310ebedc5d0725e5
Jangwoochul <dncjf6869@gmail.com>
2026-06-02 17:29:57 +0900

    예약상태

--- src/main/java/dto/ReserveStatus.java
+++ src/main/java/dto/ReserveStatus.java
@@ -1,5 +1,5 @@
 package dto;
-
+//예약상태 확인 , processReserveCancel -> 예약 취소 후 예약취소로 변경
 public class ReserveStatus {
     public static final String ACTIVE = "ACTIVE";       // 예약 중
     public static final String COMPLETED = "COMPLETED"; // 이용 완료
